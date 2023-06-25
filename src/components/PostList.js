d10c00074c00ff37842dc19f2df27bb07bb34126
KTG <xorms1919@naver.com>
2023-06-25 12:43:13 +0900

    Add build files

---
+++ src/components/PostList.js
@@ -0,0 +1,53 @@
+import { useEffect, useState } from 'react';
+
+const PostList = () => {
+  const [posts, setPosts] = useState([]);
+  const [isLoading, setIsLoading] = useState(false);
+  const [page, setPage] = useState(1);
+
+  const loadPosts = async () => {
+    setIsLoading(true);
+
+    try {
+      const response = await fetch(
+        `https://jsonplaceholder.typicode.com/posts?_page=${page}&_limit=10`
+      );
+      const data = await response.json();
+      setPosts((prevPosts) => [...prevPosts, ...data]);
+      setIsLoading(false);
+    } catch (error) {
+      console.log(error);
+      setIsLoading(false);
+    }
+  };
+
+  const handleScroll = () => {
+    const { scrollTop, clientHeight, scrollHeight } = document.documentElement;
+    if (scrollTop + clientHeight >= scrollHeight - 10) {
+      setPage((prevPage) => prevPage + 1);
+    }
+  };
+
+  useEffect(() => {
+    window.addEventListener('scroll', handleScroll);
+    return () => window.removeEventListener('scroll', handleScroll);
+  }, []);
+
+  useEffect(() => {
+    loadPosts(); // 페이지 번호 변경 시 데이터 로드
+  }, [page]);
+
+  return (
+    <div>
+      {posts.map((post) => (
+        <div key={post.id}>
+          <h3>{post.title}</h3>
+          <p>{post.body}</p>
+        </div>
+      ))}
+      {isLoading && <div>Loading...</div>}
+    </div>
+  );
+};
+
+export default PostList;
